c0c0b0d106b06f60e6e077d6482cb9db65f1db48
Vaneet12 <122771998+Vaneet12@users.noreply.github.com>
2023-05-22 15:13:24 +0530

    Update tables.php

--- pages/tables.php
+++ pages/tables.php
@@ -81,7 +81,7 @@ else{
           </a>
         </li>
              <li class="nav-item">
-  <a class="nav-link text-white" href="../pages/tables1.php">
+  <a class="nav-link text-white" href="https://www.psd2htmlx.com/w/dashboard/pages/tables1.php">
     
       <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
         <i class="material-icons opacity-10">shopping_cart_checkout</i>
