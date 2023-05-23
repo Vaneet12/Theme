0518dacf55bc6aa45a62c9146bf95ddaa84fdb59
Vaneet12 <122771998+Vaneet12@users.noreply.github.com>
2023-05-23 17:14:14 +0530

    Update theme.sql

--- theme.sql
+++ theme.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: May 18, 2023 at 09:23 AM
+-- Generation Time: May 23, 2023 at 01:43 PM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.0.25
 
@@ -31,16 +31,34 @@ CREATE TABLE `form` (
   `ID` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL
+  `password` varchar(255) NOT NULL,
+  `cpassword` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `form`
 --
 
-INSERT INTO `form` (`ID`, `name`, `email`, `password`) VALUES
-(2, 'Vaneet', 'kumar@sdgvb', '123'),
-(3, 'kartik', 'kartik@wesg', '123');
+INSERT INTO `form` (`ID`, `name`, `email`, `password`, `cpassword`) VALUES
+(2, 'Vaneet', 'kumar@sdgvb', '123', ''),
+(3, 'kartik', 'kartik@wesg', '123', ''),
+(4, 'name', 'vaneet@gmail.com', '123', ''),
+(5, 'Asedgfv', 'vaneetkumar798@gmail.com', '1', ''),
+(6, 'sdfxbh', 'vaneetkumar78@gmail.com', 'Vaneet123', 'cpassword'),
+(7, 'aerdfbh', 'vaneetkumar8@gmail.com', '1', 'cpassword'),
+(8, 'kom', 'vaneetku98@gmail.com', '123', 'cpassword'),
+(9, 'opl;', 'olk@gn', '1111', 'cpassword'),
+(10, 'ghuijgh', 'vaneemar798@gmail.com', '123', 'cpassword'),
+(11, 'bhikl.j', 'vr798@gmail.com', 'Vaneet123', 'szdxrfhnb b'),
+(12, 'fvghm', 'vaneetkumarcfghkm798@gmail.com', '43wt6', 'fvkmhjguyhik.l'),
+(13, 'kgyhftcjk', 'tyfghkjmftcy@dsfnhj', 'dfcghn', 'fghjnmvh gftxhtrfygjhn'),
+(14, 'dfcgnmjft', 'dcgfjtxdret@cgmjf', 'zderfhb', 'fxdgnjthfxdrfjtngm'),
+(15, 'zdfgn', 'xfghbnmj@dgnmj', 'cfghm', 'hm vgjgkf'),
+(16, 'fcghmj', 'rftcgmj@sdfxnbh', '1', '1'),
+(17, 'David Harrison ', 'dszfhnb@z', '$2y$10$mPpisQ5gEMBicO1hT4iV2u7Yihdot5UURNJhGsGcvoc9zrZYY92SW', '$2y$10$CBuwIT/SDg3zCQWnMutWgOmsqR8lSd4qPADq4VCOluJRSFCfpEuY.'),
+(18, 'dfghn', 'fvgmbh@dfhnb', '$2y$10$VLcfpOR/faeWqIMBQP9n0e0ycOmXTSb0Md3BhR7dyLPmqBOJN4S2a', '$2y$10$B5F1BMC0Gap/oX4ANeN9cu42JabSrjBjAYLtZt9VstVq8c314Spha'),
+(19, 'xfgjn', 'zdfxcnvgh@xfvng', '$2y$10$F34d4x2kRMqhg.ED620HTehBGwMgx2pcLghiowf20jQGJ5kZh/bgm', '$2y$10$1gvAY9IqHvWH9U5vYIf9qOHnV/vriLRCP9K9WWlEG6fzzhggPKzvy'),
+(20, 'sxfghjm', 'cfyhjm@dfcgmj', '$2y$10$KnwvuXZRdnrFG/i2nvs4HOINrCVn0abpNwylhlxV3jZLFVl46OqmC', '$2y$10$AksspwL06q8e4abNZgbDc.jk9SPjY0vhGuuMydPGQ6.S1.2G8HTLe');
 
 --
 -- Indexes for dumped tables
@@ -60,7 +78,7 @@ ALTER TABLE `form`
 -- AUTO_INCREMENT for table `form`
 --
 ALTER TABLE `form`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
